c8becea6a18c6632679524055341b20f1fa77022
Izzet Yildirim <yildirim2@llnl.gov>
2025-08-30 20:41:45 -0700

    Remove unused percentile parameter from test_e2e function

--- tests/test_main.py
+++ tests/test_main.py
@@ -75,21 +75,25 @@ def _test_e2e(
     if trace_path.endswith("darshan-posix"):
         view_types = ["file_name", "proc_name"]
 
-    dfa = init_with_hydra(
-        hydra_overrides=[
-            f"analyzer={analyzer}",
-            f"analyzer/preset={preset}",
-            f"analyzer.checkpoint={checkpoint}",
-            f"analyzer.checkpoint_dir={checkpoint_dir}",
-            "cluster=external",
-            f"cluster.restart_on_connect={True}",
-            f"cluster.scheduler_address={scheduler_address}",
-            f"hydra.run.dir={tmp_path}",
-            f"hydra.runtime.output_dir={tmp_path}",
-            f"trace_path={trace_path}",
-            f"view_types=[{','.join(view_types)}]",
-        ]
-    )
+    hydra_overrides = [
+        f"analyzer={analyzer}",
+        f"analyzer/preset={preset}",
+        f"analyzer.checkpoint={checkpoint}",
+        f"analyzer.checkpoint_dir={checkpoint_dir}",
+        "cluster=external",
+        f"cluster.restart_on_connect={True}",
+        f"cluster.scheduler_address={scheduler_address}",
+        f"hydra.run.dir={tmp_path}",
+        f"hydra.runtime.output_dir={tmp_path}",
+        f"trace_path={trace_path}",
+        f"view_types=[{','.join(view_types)}]",
+    ]
+
+    assign_epochs = analyzer == "dftracer" and preset == "dlio"
+    if assign_epochs:
+        hydra_overrides.append("analyzer.assign_epochs=True")
+
+    dfa = init_with_hydra(hydra_overrides=hydra_overrides)
 
     assert dfa.hydra_config.analyzer.checkpoint == checkpoint
     assert dfa.hydra_config.analyzer.checkpoint_dir == checkpoint_dir
